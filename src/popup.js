84a9b551a23860438dbbe81b87aa9de160d805f1
mateusneresrb <mateusneresrb@gmail.com>
2023-04-18 23:13:42 -0300

    refactor: change size in popup;

--- src/popup.js
+++ src/popup.js
@@ -4,6 +4,8 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     document.title = "Viewing theme: " + request.title;
 
     var popupImg = document.getElementById("popup-img");
+    popupImg.style.maxWidth = "650px";
+    popupImg.style.maxHeight = "300px";
     popupImg.src = request.url;
   }
 });
