46cb18342145a6dec2aab41597983753d854d66c
mateusneresrb <mateusneresrb@gmail.com>
2023-05-07 19:38:22 -0300

    refactor: some methods

--- src/popup.js
+++ src/popup.js
@@ -3,7 +3,7 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.url && request.title) {
     document.title = "Viewing theme: " + request.title;
 
-    var popupImg = document.getElementById("popup-img");
+    let popupImg = document.getElementById("popup-img");
     popupImg.style.maxWidth = "650px";
     popupImg.style.maxHeight = "300px";
     popupImg.src = request.url;
